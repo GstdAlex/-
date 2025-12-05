bc43bebf3781a6ee0275a399e283d0db557377e4
GstdAlex <xyag1619975333@qq.com>
2025-12-05 16:46:22 +0800

    Add files via upload

---
+++ 杨辉三角.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+	int n;
+	scanf_s("%d", &n);
+	int arr[20][20];
+	memset(arr, 0, sizeof(arr));
+    for (int i = 0; i <= n - 1; i++) {
+        arr[i][0] = 1;
+        arr[i][i] = 1;
+    }
+  
+for (int i = 2; i <= n - 1; i++) {
+    for (int j = 1; j <= i - 1; j++) {
+        arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+    }
+}
+int a = 0;
+for (int i = 0; i <= n - 1; i++) {
+    for (int j = 0; j <= n - 1; j++) {
+        a++;
+        printf("%d ", arr[i][j]);
+        if ((a-1) == i) {
+            printf("\n");
+			a = 0;
+            break;
+        }
+    }
+
+}
+
+
+return 0;
+}
