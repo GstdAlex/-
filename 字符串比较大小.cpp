be5e2d3b17acebb776903985f14da1cf456874fc
GstdAlex <xyag1619975333@qq.com>
2025-12-05 17:26:38 +0800

    Update 字符串比较大小.cpp

--- 字符串比较大小.cpp
+++ 字符串比较大小.cpp
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    //请在此添加‘字符串大小比较’的代码
-    /********** Begin **********/
     char a[30], b[30];
-    scanf_s("%s %s", a, (unsigned int)sizeof(a), b, (unsigned int)sizeof(b));
+    scanf("%s %s",&a,&b);
 
     int suma = 0;
     for (int i = 0;; i++) {
@@ -18,6 +16,5 @@ int main()
     }
     printf("%d", suma - sumb);
 
-    /********** End **********/
     return 0;
 }
