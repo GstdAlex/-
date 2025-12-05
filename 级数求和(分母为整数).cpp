bf990422baa89ca8e8661f12ac0e27e64e7823c7
GstdAlex <xyag1619975333@qq.com>
2025-12-05 17:17:57 +0800

    Clarify floating-point division in series sum calculation
    Added a comment to clarify the purpose of floating-point division.

--- 级数求和(分母为整数).cpp
+++ 级数求和(分母为整数).cpp
@@ -6,7 +6,7 @@ int main(){
 	int b,c;
 	double s=0;
     for(int i=1;;i++){
-    	s+=1.0/i;
+    	s+=1.0/i; //设计为浮点数除法
     	if(s>a){
     		printf("%d",i);
     		break;
@@ -14,3 +14,4 @@ int main(){
 	}
 	return 0; 
 } 
+
